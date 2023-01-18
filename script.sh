da1140d642d8ddcf185ebb831e794beff875c67a
darega <darega@floridauniversitaria.es>
2023-01-18 19:25:24 +0100

    compruebo num romano y agrego bucle

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 numero=$1
 es_numero=^[0-9]+$
-tamanyo=`echo $numero | wc -c`
-tamanyo=$((tamanyo-1))
-max=$((tamanyo-1))
+es_romano=^[M/C/D/V/I/X/L]+$
+
+tamanyo=$(echo $numero | wc -c)
+tamanyo=$((tamanyo - 1))
+max=$((tamanyo - 1))
 
 if [[ $numero =~ $es_numero ]]; then
     if [ $numero -ge 1 -a $numero -le 1999 ]; then
@@ -11,4 +13,15 @@ if [[ $numero =~ $es_numero ]]; then
     else
         echo "El numero introducio esta fuera de rango. Debe de estar entre 1 y 1999."
     fi
+else
+    if [[ $numero =~ $es_romano ]]; then
+        echo "funciona perfe"
+    else
+        echo "No es romano."
+    fi
 fi
+
+for i in seq 0 $max; do
+    echo "${numero:$i:1}"
+    sleep 0.2s
+done
