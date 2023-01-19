3d9f41648eaa3b04575511bc3ef01ca603ddb373
darega <darega@floridauniversitaria.es>
2023-01-19 14:21:11 +0100

    detalles

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@ es_numero=^[0-9]+$
 es_romano=^[M/C/D/V/I/X/L]+$
 
 tamanyo=$(echo $numero | wc -c)
-tamanyo=$((tamanyo - 1))
-max=$((tamanyo - 1))
+tamanyo=$((tamanyo - 1)) #tamanyo=numero de caracteres.
+max=$((tamanyo - 1))     #ultimo elemtno del string
 
 if [[ $numero =~ $es_numero ]]; then
     if [ $numero -ge 1 -a $numero -le 1999 ]; then
@@ -23,5 +23,9 @@ fi
 
 for i in seq 0 $max; do
     echo "${numero:$i:1}"
+    if [ ]; then
+    # metodo coneversion / devuelve solucion
+    # conversion=solucion
+    fi
     sleep 0.2s
 done
